7533443d8fd1b87f583d982b0130371663de97dd
devanaclimgo <anaclimgo@gmail.com>
2025-07-20 15:21:53 -0300

    refactor: styles dentro do classname na view

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -10,16 +10,11 @@ type ScreenContentProps = {
 
 export const ScreenContent = ({ title, path, children }: ScreenContentProps) => {
   return (
-    <View className={styles.container}>
-      <Text className={styles.title}>{title}</Text>
-      <View className={styles.separator} />
+    <View className="items-center flex-1 justify-center">
+      <Text className="text-xl font-bold">{title}</Text>
+      <View className="h-[1px] my-7 w-4/5 bg-gray-200" />
       <EditScreenInfo path={path} />
       {children}
     </View>
   );
-};
-const styles = {
-  container: `items-center flex-1 justify-center`,
-  separator: `h-[1px] my-7 w-4/5 bg-gray-200`,
-  title: `text-xl font-bold`,
 };
