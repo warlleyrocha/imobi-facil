3fd2842d8e3c93831d01adad0d5d70f9dcab97cd
Warlley Rocha <warlleyrocha@icloud.com>
2025-07-22 10:47:34 -0300

    feat: telas feedback e instalar libs de svg

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,22 +1,26 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, ImageBackground } from "react-native";
 
 export default function SignIn() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-        <View className="flex-1 items-center justify-center">
+        <ImageBackground 
+        source={require("../../assets/background-login.jpg")}
+        className="flex-1 items-center justify-center w-[375px] h-[410px]"
+        >
             <Image
             source={require("../../assets/splash.png")}/>
-            <Text>
+            <Text className="text-[24px] font-bold text-white ">
                 Bem vindo ao ImobiFácil
             </Text>
-        </View>
+        </ImageBackground>
 
-        <View className="flex-1 items-center justify-center">
-            <Text>
+        // Container para o conteúdo do login
+        <View className="flex-1 px-[16px] items-center justify-center rounded-t-[30px] -mt-[10px] z-10 bg-white gap-[10px]">
+            <Text className="text-[20px] text-[#111928] font-bold">
                 Conheça nosso APP
             </Text>
 
-            <Text>
+            <Text className="text-[14px] text-[#111928] text-center">
             Faça login de forma rápida e segura usando sua conta Google. Assim você acessa o ImobiFácil sem precisar lembrar de senhas.
             </Text>
 
