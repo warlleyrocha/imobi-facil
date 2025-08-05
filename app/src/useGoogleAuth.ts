f3a3ff9acdf3f74f4f2bd2c86fac3c7496facc5d
devanaclimgo <anaclimgo@gmail.com>
2025-08-05 19:27:46 -0300

    feat: client_id, uri localhost

---
+++ app/src/useGoogleAuth.ts
@@ -0,0 +1,25 @@
+import * as Google from 'expo-auth-session/providers/google'
+import * as WebBrowser from 'expo-web-browser'
+import { useEffect } from 'react'
+import * as AuthSession from 'expo-auth-session';
+
+WebBrowser.maybeCompleteAuthSession()
+
+const CLIENT_ID = '880603979575-5lvjao2oitb9rrt91atr5gmttu8ol5aa.apps.googleusercontent.com '
+
+export const useGoogleAuth = () => {
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: CLIENT_ID,
+    redirectUri: 'http://localhost:8081',
+    scopes: ['profile', 'email'],
+  })
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { authentication } = response
+      console.log('Google Access Token:', authentication?.accessToken)
+    }
+  }, [response])
+
+  return { request, response, promptAsync }
+}
