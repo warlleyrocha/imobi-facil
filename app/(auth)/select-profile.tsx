2e2a37eef434873092dada09c5cd9caae2c1585e
Warlley Rocha <warlleyrocha@icloud.com>
2025-08-07 10:36:32 -0300

    feat: start

---
+++ app/(auth)/select-profile.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Button, Image } from "react-native";
+
+export default function SelectProfile() {
+  return (
+    <View>
+      <Text>Select Profile</Text>
+
+      <Button title="Corretor" />
+      <Button title="Usuario" />
+    </View>
+  );
+}
