5ccd67dd41612425b84069a18265257f2106f456
devanaclimgo <anaclimgo@gmail.com>
2025-08-14 19:07:42 -0300

    feat: adicionado header tela do corretor

--- app/(auth)/corretor/index.tsx
+++ app/(auth)/corretor/index.tsx
@@ -1,9 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import { Container } from "~/components/Container";
 
 export default function Corretor() {
+  const setaEsquerda = require("~/assets/arrow-left.png");
+  
   return (
     <View>
-      <Text>Tela inicial do corretor</Text>
+      <Container>
+        <View className="items-center flex-1 justify-center">
+          <View className="flex-row items-center gap-12 justify-between">
+            <Image source={setaEsquerda} style={{ width: 24, height: 24 }} className="text-left" />
+            <Text className="text-xl font-bold text-center">Cadastro do Corretor</Text>
+          </View>
+        </View>
+      </Container>
     </View>
   );
 }
