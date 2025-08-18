8e1eca1d167defd0fb07e63cab75894e8c46738b
devanaclimgo <anaclimgo@gmail.com>
2025-08-18 14:59:33 -0300

    fix: correção de elementos html para react native refactor: refatoração de event handlers feat: implementações nativas fix: propriedades que não existem no react native removidas style: adicionado flex-1 e bg-white no view pai

--- app/(auth)/corretor/index.tsx
+++ app/(auth)/corretor/index.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
 import { Container } from '~/components/Container';
-import axios from 'axios';
-import { useRouter } from 'expo-router';
+import { router } from 'expo-router';
 
 export default function Corretor() {
   const setaEsquerda = require('~/assets/arrow-left.png');
@@ -29,21 +28,11 @@ export default function Corretor() {
     creci: '',
   });
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({ ...prevData, [name]: value })); // Atualiza o estado do formulário com o novo valor
-  };
-
-  const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
-    // Previne o comportamento padrão do formulário
-    e.preventDefault();
+  const handleSubmit = async () => {
     try {
-      const response = await axios.post('http://localhost:8081/apiv1/signup', formData);
-      console.log(response); // Exibe a resposta da requisição no console
-      // Redireciona após cadastro bem-sucedido
-      window.location.href = '/(auth)/verificacao';
+      router.push('/(auth)/corretor/verificacao' as any);
     } catch (error) {
-      console.error(error); // Exibe o erro no console
+      console.error(error);
     }
   };
 
@@ -53,13 +42,8 @@ export default function Corretor() {
     setIsChecked(!isChecked);
   };
 
-  const router = useRouter();
-  function handleBack() {
-    router.push('/(auth)/corretor/verificacao'); // Navega para a página anterior
-  }
-
   return (
-    <View className="m-0 p-0">
+    <View className="m-0 p-0 flex-1 bg-white">
       <Container>
         <View className="w-full flex-1 items-start justify-center">
           <View className="mt-10 flex-row items-center justify-between gap-12">
@@ -67,121 +51,135 @@ export default function Corretor() {
             <Text className="text-center text-xl font-bold">Cadastro do Corretor</Text>
           </View>
           <View>
-            <form className="full-width mt-8 w-full" onSubmit={handleSubmit}>
-              <div>
-                <label className="form-title mb-2 block text-sm font-medium text-gray-700">
-                  Nome*
-                </label>
-                <input
-                  type="text"
-                  name="firstname"
-                  value={formData.firstname}
-                  onChange={handleChange}
-                  required
-                  maxLength={50}
-                  className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
-                  placeholder="Digite o seu Nome"
-                />
-              </div>
+            <TouchableOpacity className="full-width mt-8 w-full">
+              <View>
+                <View className="full-width mt-8 w-full">
+                  <View>
+                    <Text className="form-title mb-2 block text-sm font-medium text-gray-700">
+                      Nome*
+                    </Text>
+                    <TextInput
+                      value={formData.firstname}
+                      onChangeText={(value) =>
+                        setFormData((prevData) => ({ ...prevData, firstname: value }))
+                      }
+                      className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
+                      placeholder="Digite o seu Nome"
+                    />
+                  </View>
+                  <Text className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
+                    Sobrenome*
+                  </Text>
+                  <TextInput
+                    value={formData.lastname}
+                    onChangeText={(value) =>
+                      setFormData((prevData) => ({ ...prevData, lastname: value }))
+                    }
+                    className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
+                    placeholder="Digite o seu Nome Completo"
+                  />
+                </View>
 
-              <div>
-                <label className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
-                  Sobrenome*
-                </label>
-                <input
-                  type="text"
-                  name="lastname"
-                  value={formData.lastname}
-                  onChange={handleChange}
-                  required
-                  maxLength={50}
-                  className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
-                  placeholder="Digite o seu Nome Completo"
-                />
-              </div>
+                <View>
+                  <Text className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
+                    Data de Nascimento*
+                  </Text>
+                  <TextInput
+                    value={String(formData.birthdate)}
+                    onChangeText={(value) =>
+                      setFormData((prevData) => ({ ...prevData, birthdate: value }))
+                    }
+                    className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
+                    placeholder="DD/MM/AAAA"
+                  />
+                </View>
+                <View>
+                  <Text className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
+                    CPF*
+                  </Text>
+                  <TextInput
+                    value={String(formData.cpf)}
+                    onChangeText={(value) =>
+                      setFormData((prevData) => ({ ...prevData, cpf: value }))
+                    }
+                    keyboardType="numeric"
+                    className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
+                    placeholder="000.000.000-00"
+                  />
+                </View>
 
-              <div>
-                <label className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
-                  Data de Nascimento*
-                </label>
-                <input
-                  type="date"
-                  name="birthdate"
-                  value={formData.birthdate}
-                  onChange={handleChange}
-                  required
-                  className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
-                  placeholder="DD/MM/AAAA"
-                />
-              </div>
-              <div>
-                <label className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
-                  CPF*
-                </label>
-                <input
-                  type="text"
-                  name="cpf"
-                  value={formData.cpf}
-                  onChange={handleChange}
-                  required
-                  maxLength={11}
-                  className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
-                  placeholder="000.000.000-00"
-                />
-              </div>
-
-              <div>
-                <label className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
-                  CRECI/Estado*
-                </label>
-                <input
-                  type="text"
-                  name="creci"
-                  value={formData.creci}
-                  onChange={handleChange}
-                  required
-                  maxLength={20}
-                  className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
-                  placeholder="Digite o seu CRECI"
-                />
-              </div>
-            </form>
-          </View>
-          <View className="mt-32 flex w-full justify-end">
-            <div className="flex items-center gap-2 text-center">
-              <label>
-                <input
-                  type="checkbox"
-                  checked={isChecked}
-                  onChange={handleCheckboxChange}
-                  className="h-6 w-6 appearance-none rounded text-cor-primaria"
-                />
-              </label>
-              <Text className="font-mulish-light text-sm text-gray-500">
-                Concordo com os nossos{' '}
-                <a
-                  href="/termos"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-cor-primaria underline">
-                  Termos
-                </a>{' '}
-                e{' '}
-                <a
-                  href="/politicas"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-cor-primaria underline">
-                  Políticas.
-                </a>{' '}
-              </Text>
-            </div>
-            <button
-              type="submit"
-              className="btn-azul mt-4 w-full rounded p-3 font-mulish-medium text-base text-white"
-              onClick={handleBack}>
-              Cadastrar
-            </button>
+                <View>
+                  <Text className="form-title mb-2 mt-4 block text-sm font-medium text-gray-700">
+                    CRECI/Estado*
+                  </Text>
+                  <TextInput
+                    value={formData.creci}
+                    onChangeText={(value) =>
+                      setFormData((prevData) => ({ ...prevData, creci: value }))
+                    }
+                    className="form-input:focus w-full rounded border border-gray-300 p-2 text-black"
+                    placeholder="Digite o seu CRECI"
+                  />
+                </View>
+              </View>
+            </TouchableOpacity>
+            <View className="mt-32 flex w-full justify-end">
+              <View className="flex flex-row items-center gap-2 text-center">
+                <TouchableOpacity onPress={handleCheckboxChange} style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  <View
+                    style={{
+                      height: 24,
+                      width: 24,
+                      borderRadius: 4,
+                      borderWidth: 2,
+                      borderColor: '#81b0ff',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      backgroundColor: isChecked ? '#007bff' : '#fff',
+                    }}
+                  >
+                    {isChecked && (
+                      // Ícone de "certo" usando SVG
+                      <View style={{ position: 'absolute', left: 2, top: 1 }}>
+                        <svg width="16" height="16" viewBox="0 0 16 16">
+                          <polyline
+                            points="3,9 7,13 13,5"
+                            fill="none"
+                            stroke="#fff"
+                            strokeWidth="2"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </View>
+                    )}
+                  </View>
+                </TouchableOpacity>
+                <Text className="font-mulish-light text-sm text-gray-500">
+                  Concordo com os nossos{' '}
+                  <a
+                    href="/termos"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-cor-primaria underline">
+                    Termos
+                  </a>{' '}
+                  e{' '}
+                  <a
+                    href="/politicas"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-cor-primaria underline">
+                    Políticas.
+                  </a>{' '}
+                </Text>
+              </View>
+              <TouchableOpacity
+                className="btn-azul mt-4 w-full rounded p-3 font-mulish-medium text-base text-white"
+                onPress={handleSubmit}>
+                Cadastrar
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </Container>
