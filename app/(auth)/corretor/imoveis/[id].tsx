51a2463266254c1baac7f38f62cd80c490a80f42
warlleyrocha <warlleyrocha@icloud.com>
2025-09-04 12:51:52 -0300

    feat: criado arquivos referentes ao fluxo de imóveis cadastrados

---
+++ app/(auth)/corretor/imoveis/[id].tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+export default function DetailsImovel() {
+  return (
+    <View>
+      <Text>Detalhes do imóvel</Text>
+    </View>
+  );
+}
---
+++ app/(auth)/corretor/imoveis/new.tsx
@@ -0,0 +1,8 @@
+import { View, Text } from 'react-native';
+export default function FormImovel() {
+  return (
+    <View>
+      <Text>Novo Imovel Page</Text>
+    </View>
+  );
+}
