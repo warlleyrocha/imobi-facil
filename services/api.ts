972fbe7e16446f7ac8c80c35972ea6552d909ed7
warlleyrocha <warlleyrocha@icloud.com>
2025-09-07 11:43:10 -0300

    feat: criar instância

---
+++ services/api.ts
@@ -0,0 +1,11 @@
+// Importar a dependência Axios para conectar com a API
+import axios from 'axios';
+
+// Criar a instância de conexão HTTP
+const api = axios.create({
+  baseURL: ' http://localhost:3333/', // URL do banco de dados
+  timeout: 5000,
+});
+
+// Exporta a instância configurada do Axios
+export default api;
