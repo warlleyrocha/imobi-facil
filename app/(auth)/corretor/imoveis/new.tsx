a019fa400eeab00205d0769c5a87b3879fa1e10c
devanaclimgo <anaclimgo@gmail.com>
2025-09-08 09:16:17 -0300

    feat: adicionado link temporário para página de 'addSucess'

--- app/(auth)/corretor/imoveis/new.tsx
+++ app/(auth)/corretor/imoveis/new.tsx
@@ -1,8 +1,14 @@
+import { Link } from 'expo-router';
 import { View, Text } from 'react-native';
 export default function FormImovel() {
   return (
     <View>
       <Text>Novo Imovel Page</Text>
+
+      {/* Link temporário para success page */}
+      <Link href="/(auth)/corretor/imoveis/addSuccess">
+        <Text className='text-blue-600'>Go to Success Page</Text>
+      </Link>
     </View>
   );
 }
