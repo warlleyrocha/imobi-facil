553d295c6960ec1f04c1bd5a2bef37c1d5080382
warlleyrocha <warlleyrocha@icloud.com>
2025-10-11 16:38:39 -0300

    test: testa api

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -4,22 +4,21 @@ import { GoogleIcon } from '@/components/Icons/GoogleIcon';
 import { useGoogleAuth } from '@/hooks/useGoogleAuth';
 
 const Auth = () => {
-  const { loginWithGoogle, loading, error } = useGoogleAuth();
+  const { loginWithGoogle, isLoading } = useGoogleAuth();
 
   return (
     <View>
       <TouchableOpacity
-        disabled={loading}
+        disabled={isLoading}
         onPress={loginWithGoogle}
         className="h-[50px] w-[345px] flex-row items-center justify-center gap-[19px] rounded-[5px] bg-[#E6E6E6]">
         <GoogleIcon size={20} />
-        {loading ? (
+        {isLoading ? (
           <ActivityIndicator size="small" color="#111928" />
         ) : (
           <Text className="font-inter-light text-[#111928]">Continue com o Google</Text>
         )}
       </TouchableOpacity>
-      {error && <Text className="mt-2 text-red-600">{error}</Text>}
     </View>
   );
 };
--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,7 @@
 //services/api.ts
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'http://192.168.1.20:3000';
 
 export const api = axios.create({
   baseURL: API_URL,
