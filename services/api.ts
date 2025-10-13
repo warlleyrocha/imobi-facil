2bde0d096cde7727a093b922e555e2b370ccc0fc
warlleyrocha <warlleyrocha@icloud.com>
2025-10-13 20:14:34 -0300

    feat: implementa fluxo mobile

--- services/api.ts
+++ services/api.ts
@@ -1,7 +1,12 @@
 //services/api.ts
 import axios from 'axios';
+import { Platform } from 'react-native';
 
-export const API_URL = 'http://localhost:3000';
+export const API_URL = Platform.select({
+  android: 'http://10.0.2.2:3000',
+  ios: 'http://192.168.1.1:3000',
+  web: 'http://localhost:3000',
+}) as string;
 
 export const api = axios.create({
   baseURL: API_URL,
