addee5b783cf95318d6126aba56647a9a36f4acf
Fr4nc021 <164265892+Fr4nc021@users.noreply.github.com>
2025-10-15 21:40:37 -0300

    feat:  adicione  máscara  nas  entradas  da  tela  de  cadastro

--- components/Form/FormInput.tsx
+++ components/Form/FormInput.tsx
@@ -10,11 +10,12 @@ export function FormInput({ label, required, containerClassName, ...props }: For
   return (
     <View className={`gap-[10px] pb-[12px] ${containerClassName ?? ""}`}>
       <Text className="font-mulish-medium text-[16px] text-dark-5">
-        {label}{required && "*"}
+        {label.replace('*', '')}
+        {label.includes(`*`) && <Text style={{ color: 'red' }}> *</Text>}
       </Text>
       <TextInput
         className="h-[48px] font-mulish rounded-[8px] text-[16px] border border-stroke bg-white py-[12px] pl-[20px] pr-[16px]"
-        placeholderTextColor="#9CA3AF"
+        placeholderTextColor="#9CA3AF" 
         style={{ color: "#1F2A37", fontSize: 16 }}
         {...props}
       />
