eabc2d20d87837be98900f1e7700c980c7d5b9c9
warlleyrocha <warlleyrocha@icloud.com>
2025-10-18 17:11:35 -0300

    fix: corrige comportamentos dos botões

--- app/(auth)/onboard.tsx
+++ app/(auth)/onboard.tsx
@@ -44,10 +44,12 @@ export default function Onboard() {
     router.replace('/(auth)/corretor/(tabs)/home');
   };
 
+  const isFirstSlide = currentIndex === 0;
+
   return (
     <View className="flex-1 bg-white">
       {/* Container para o ImageSlide */}
-      <View className="flex-1 pb-24 pt-12">
+      <View className="flex-1 pb-24 pt-[50px]">
         <ImageSlide
           images={onboardImages}
           currentIndex={currentIndex}
@@ -60,36 +62,39 @@ export default function Onboard() {
       {/* Container dos botões fixo no final */}
       {currentIndex === onboardImages.length - 1 ? (
         // Último slide - botão único centralizado
-        <View className=" items-center justify-center px-4 pb-8">
+        <View className=" items-center justify-center px-4 pb-[40px]">
           <TouchableOpacity
-            className="w-full flex-row items-center justify-center gap-2 rounded-lg bg-cor-primaria px-[24px] py-[12px]"
+            className="w-full flex-row items-center justify-center gap-2 rounded-lg bg-cor-primaria px-[24px] py-[10px]"
             accessibilityRole="button"
             onPress={handleStart}>
-            <Text className="font-mulish-medium text-base font-medium text-white md:text-lg">
+            <Text className="font-mulish-medium text-[16px] font-medium text-white md:text-lg">
               Começar Agora
             </Text>
           </TouchableOpacity>
         </View>
       ) : (
         // Slides anteriores - dois botões
-        <View className="absolute bottom-8 left-0 right-0 flex-row justify-between px-4 md:bottom-12 md:px-5">
-          <TouchableOpacity
-            className="items-center justify-center rounded-full bg-cor-primaria p-4"
-            accessibilityRole="button"
-            onPress={handleBack}>
-            <Svg width={21} height={20} viewBox="0 0 21 20" fill="none">
-              <Path
-                d="M18 9.3125H4.65625L9.96875 3.90625C10.25 3.625 10.25 3.1875 9.96875 2.90625C9.6875 2.625 9.25 2.625 8.96875 2.90625L2.5 9.46875C2.21875 9.75 2.21875 10.1875 2.5 10.4688L8.96875 17.0312C9.09375 17.1563 9.28125 17.25 9.46875 17.25C9.65625 17.25 9.8125 17.1875 9.96875 17.0625C10.25 16.7812 10.25 16.3438 9.96875 16.0625L4.6875 10.7187H18C18.375 10.7187 18.6875 10.4062 18.6875 10.0312C18.6875 9.625 18.375 9.3125 18 9.3125Z"
-                fill="#FAFAFA"
-              />
-            </Svg>
-          </TouchableOpacity>
+        <View
+          className={`absolute bottom-10 left-0 right-0 flex-row px-4 md:bottom-12 md:px-5 ${isFirstSlide ? 'justify-end' : 'justify-between'}`}>
+          {!isFirstSlide && (
+            <TouchableOpacity
+              className="items-center justify-center rounded-full bg-cor-primaria p-4"
+              accessibilityRole="button"
+              onPress={handleBack}>
+              <Svg width={21} height={20} viewBox="0 0 21 20" fill="none">
+                <Path
+                  d="M18 9.3125H4.65625L9.96875 3.90625C10.25 3.625 10.25 3.1875 9.96875 2.90625C9.6875 2.625 9.25 2.625 8.96875 2.90625L2.5 9.46875C2.21875 9.75 2.21875 10.1875 2.5 10.4688L8.96875 17.0312C9.09375 17.1563 9.28125 17.25 9.46875 17.25C9.65625 17.25 9.8125 17.1875 9.96875 17.0625C10.25 16.7812 10.25 16.3438 9.96875 16.0625L4.6875 10.7187H18C18.375 10.7187 18.6875 10.4062 18.6875 10.0312C18.6875 9.625 18.375 9.3125 18 9.3125Z"
+                  fill="#FAFAFA"
+                />
+              </Svg>
+            </TouchableOpacity>
+          )}
 
           <TouchableOpacity
-            className="w-[140px] flex-row items-center gap-2 rounded-lg bg-cor-primaria px-6 py-3"
+            className="w-[140px] flex-row items-center justify-center gap-3 rounded-lg bg-cor-primaria px-6 py-[10px]"
             accessibilityRole="button"
             onPress={handleNext}>
-            <Text className="font-mulish-medium text-base font-medium text-white md:text-lg">
+            <Text className="font-mulish-medium text-[16px] font-medium text-white md:text-lg">
               Próximo
             </Text>
             <Svg width={20} height={20} viewBox="0 0 20 20" fill="none">
